78c326eaeeb92ec59a65f0c50b6039f85645c54c
MrJimb0727 <caydentheclown27@gmail.com>
2025-11-06 16:39:28 -0500

    game files (woah)

---
+++ player.gd
@@ -0,0 +1,9 @@
+extends Sprite2D
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name != "player":
+		return
+	call_deferred("reload_current_scene")
+
+func reload_current_scene():
+	get_tree().reload_current_scene()
